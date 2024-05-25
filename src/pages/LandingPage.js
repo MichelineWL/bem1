145024b00525a937b735f64aeef95ba3f42b8770
MichelineWL <michelinewijaya@gmail.com>
2024-05-25 19:52:39 +0700

    debug: change the bg image on landing page to w-full h-full

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,9 +4,9 @@ import bg1 from '../images/bg1.webp';
 
 const LandingPage = () => {
     return (
-        <div className='relative h-screen flex items-center justify-center bg-blue-100'>
+        <div className='relative h-screen flex items-center justify-center'>
             <div className='relative flex flex-col items-center'>
-                <img src={bg1} alt='Background' className='object-contain rounded-2xl' />
+                <img src={bg1} alt='Background' className='object-contain rounded-2xl w-full h-full' />
                 <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2'>
                     <Link to='/Login'>
                         <button className='w-60 h-14 border-2 border-white bg-purple-900 bg-opacity-60 text-2xl text-white font-semibold rounded-full hover:font-bold'>
